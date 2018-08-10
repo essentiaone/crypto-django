d93912bcb96b69654905cbcda2b26c8a3e03488a
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-08-10 20:41:15 +0300

    Create Ethreum wallet form field

--- crypto_django/forms/fields/addresses/ethereum.py
+++ crypto_django/forms/fields/addresses/ethereum.py
@@ -1,23 +1,54 @@
 """
-Docs.
+Implementation for Ethereum address form field.
 """
+from eth_utils import is_address
 from django import forms
+from django.core.exceptions import ValidationError
+
+from crypto_django.constants.address import (
+    HEX_PREFIX,
+    REQUIRED_ETHEREUM_ADDRESS_LENGTH,
+)
 
 
 class EthereumAddressField(forms.CharField):
     """
-    Docs.
+    Ethereum address form field implementation.
     """
 
     default_error_messages = {
         'invalid': 'Invalid Ethereum address.',
+        'length':  'Ensure address has %(required_address_length)d character (it has %(current_address_length)d).',
+        'hex': 'Ensure address has \'0x\' as two first characters.',
     }
 
     def __init__(self, *args, **kwargs):
         super(EthereumAddressField, self).__init__(*args, **kwargs)
 
-    def to_python(self, value):
+    def to_python(self, address):
         """
-        Docs.
+        Validate Ethereum address.
+
+        References:
+            - github.com/ethereum/eth-utils#is_addressvalue---bool
+            - github.com/ethereum/eth-utils/blob/a5569fdac5f0e7575872ca3aad7802068974de6b/eth_utils/address.py#L55
+            - github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
         """
-        return super().to_python(value)
+        address_length = len(address)
+        address_first_two_characters = address[:2]
+
+        if not address_length == REQUIRED_ETHEREUM_ADDRESS_LENGTH:
+            error_message_params = {
+                'required_address_length': REQUIRED_ETHEREUM_ADDRESS_LENGTH,
+                'current_address_length': address_length,
+            }
+
+            raise ValidationError(self.error_messages.get('length'), code='length', params=error_message_params)
+
+        if not address_first_two_characters == HEX_PREFIX:
+            raise ValidationError(self.error_messages.get('hex'), code='hex')
+
+        if not is_address(address):
+            raise ValidationError(self.error_messages.get('invalid'), code='invalid')
+
+        return super().to_python(address)
