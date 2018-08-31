2d7554763198e891cd609f35b7fe1fb0fb0f09cb
Sergey Zaharov <sszaharovd@gmail.com>
2018-08-31 13:15:36 +0300

    Add abstract class for bitcoin and litecoin

--- crypto_django/forms/fields/addresses/bitcoin.py
+++ crypto_django/forms/fields/addresses/bitcoin.py
@@ -1,10 +1,6 @@
 """
 Provide implementation for Bitcoin address form field.
 """
-from bit.base58 import b58decode_check
-from django import forms
-from django.core.exceptions import ValidationError
-
 from crypto_django.constants.address import (
     BITCOIN_BECH32_PREFIX,
     BITCOIN_P2SH_PREFIX,
@@ -12,56 +8,45 @@ from crypto_django.constants.address import (
     MIN_BITCOIN_ADDRESS_LENGTH,
     REQUIRED_BITCOIN_ADDRESS_LENGTH,
 )
-from crypto_django.forms.fields.bech32 import Bech32
+from crypto_django.forms.abc.fields.addresses.bitcoin import AbstractAddressField
 
 
-class BitcoinAddressField(forms.CharField):
+class BitcoinAddressField(AbstractAddressField):
     """
     Bitcoin address form field implementation.
     """
 
-    default_error_messages = {
-        'bech32': 'Invalid bech32 address',
-        'length': 'Ensure address has %(required_address_length)d character (it has %(current_address_length)d).',
-        'p2': 'Invalid P2PKH/P2SH address. %(b58decode_error)s',
-        'prefix': 'Invalid address prefix - it has to start with one of the [1, 3, bc1]',
-    }
-
-    def to_python(self, value):
+    @property
+    def bech32_prefix(self):
         """
-        Validate Bitcoin address.
-
-        References:
-            - https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
-            - https://github.com/ofek/bit/blob/e5640cbc79c183b8c4051b46b3ccf3e813e021d3/bit/base58.py
-            - https://github.com/sipa/bech32/blob/master/ref/python/segwit_addr.py
+        Initialize bech32 address prefix.
         """
-        address = value
-        address_length = len(address)
-        prefixes = [BITCOIN_P2SH_PREFIX, BITCOIN_P2PKH_PREFIX, BITCOIN_BECH32_PREFIX]
+        return BITCOIN_BECH32_PREFIX
 
-        if MIN_BITCOIN_ADDRESS_LENGTH < address_length < REQUIRED_BITCOIN_ADDRESS_LENGTH:
-            error_message_params = {
-                'required_address_length': REQUIRED_BITCOIN_ADDRESS_LENGTH,
-                'current_address_length': address_length,
-            }
-            raise ValidationError(self.error_messages.get('length'), code='length', params=error_message_params)
+    @property
+    def p2sh_prefix(self):
+        """
+        Initialize p2sh address prefix.
+        """
+        return BITCOIN_P2SH_PREFIX
 
-        if not any([address.startswith(prefix) for prefix in prefixes]):
-            raise ValidationError(self.error_messages.get('prefix'), code='prefix')
+    @property
+    def p2pkh_prefix(self):
+        """
+        Initialize p2pkh address prefix.
+        """
+        return BITCOIN_P2PKH_PREFIX
 
-        if address.startswith(BITCOIN_BECH32_PREFIX) and not Bech32().bech32_decode(address):
-            # Checksum validation for segwit addresses
-            raise ValidationError(self.error_messages.get('bech32'), code='bech32')
+    @property
+    def min_address_length(self):
+        """
+        Initialize minimal address length.
+        """
+        return MIN_BITCOIN_ADDRESS_LENGTH
 
-        if address.startswith(BITCOIN_P2SH_PREFIX) or address.startswith(BITCOIN_P2PKH_PREFIX):
-            # Checksum validation for P2KH/P2SH addresses
-            try:
-                b58decode_check(value)
-            except ValueError as error_message:
-                error_message_params = {
-                    'b58decode_error': error_message,
-                }
-                raise ValidationError(self.error_messages.get('p2'), code='p2', params=error_message_params)
-
-        return super().to_python(address)
+    @property
+    def required_address_length(self):
+        """
+        Initialize maximal address length.
+        """
+        return REQUIRED_BITCOIN_ADDRESS_LENGTH
--- crypto_django/forms/fields/addresses/litecoin.py
+++ crypto_django/forms/fields/addresses/litecoin.py
@@ -1,10 +1,6 @@
 """
 Provide implementation for Litecoin address form field.
 """
-from bit.base58 import b58decode_check
-from django import forms
-from django.core.exceptions import ValidationError
-
 from crypto_django.constants.address import (
     LITECOIN_BECH32_PREFIX,
     LITECOIN_P2SH_PREFIX,
@@ -12,57 +8,45 @@ from crypto_django.constants.address import (
     MIN_LITECOIN_ADDRESS_LENGTH,
     REQUIRED_LITECOIN_ADDRESS_LENGTH,
 )
-from crypto_django.forms.fields.bech32 import Bech32
+from crypto_django.forms.abc.fields.addresses.bitcoin import AbstractAddressField
 
 
-class LitecoinAddressField(forms.CharField):
+class LitecoinAddressField(AbstractAddressField):
     """
     Litecoin address form field implementation.
     """
 
-    default_error_messages = {
-        'bech32': 'Invalid bech32 address',
-        'length': 'Ensure address has %(required_address_length)d character (it has %(current_address_length)d).',
-        'p2': 'Invalid P2PKH/P2SH address. %(b58decode_error)s',
-        'prefix': 'Invalid address prefix - it has to start with one of the [L, M, ltc1]',
-    }
-
-    def to_python(self, value):
+    @property
+    def bech32_prefix(self):
         """
-        Validate Litecoin address.
-
-        References:
-            - https://blog.trezor.io/litecoins-new-p2sh-segwit-addresses-843633e3e707
-            - https://github.com/litecoin-project/litecoin/issues/312
-            - https://github.com/sipa/bech32/blob/master/ref/python/segwit_addr.py
-            - https://chaining.ru/2018/05/31/litecoin-core-v0-16-0-release-litecoin-project/
+        Initialize bech32 address prefix.
         """
-        address = value
-        address_length = len(address)
-        prefixes = [LITECOIN_P2SH_PREFIX, LITECOIN_P2PKH_PREFIX, LITECOIN_BECH32_PREFIX]
+        return LITECOIN_BECH32_PREFIX
 
-        if MIN_LITECOIN_ADDRESS_LENGTH < address_length < REQUIRED_LITECOIN_ADDRESS_LENGTH:
-            error_message_params = {
-                'required_address_length': REQUIRED_LITECOIN_ADDRESS_LENGTH,
-                'current_address_length': address_length,
-            }
-            raise ValidationError(self.error_messages.get('length'), code='length', params=error_message_params)
+    @property
+    def p2sh_prefix(self):
+        """
+        Initialize p2sh address prefix.
+        """
+        return LITECOIN_P2SH_PREFIX
 
-        if not any([address.startswith(prefix) for prefix in prefixes]):
-            raise ValidationError(self.error_messages.get('prefix'), code='prefix')
+    @property
+    def p2pkh_prefix(self):
+        """
+        Initialize p2pkh address prefix.
+        """
+        return LITECOIN_P2PKH_PREFIX
 
-        if address.startswith(LITECOIN_BECH32_PREFIX) and not Bech32().bech32_decode(address):
-            # Checksum validation for segwit addresses
-            raise ValidationError(self.error_messages.get('bech32'), code='bech32')
+    @property
+    def min_address_length(self):
+        """
+        Initialize minimal address length.
+        """
+        return MIN_LITECOIN_ADDRESS_LENGTH
 
-        if address.startswith(LITECOIN_P2SH_PREFIX) or address.startswith(LITECOIN_P2PKH_PREFIX):
-            # Checksum validation for P2KH/P2SH addresses
-            try:
-                b58decode_check(value)
-            except ValueError as error_message:
-                error_message_params = {
-                    'b58decode_error': error_message,
-                }
-                raise ValidationError(self.error_messages.get('p2'), code='p2', params=error_message_params)
-
-        return super().to_python(address)
+    @property
+    def required_address_length(self):
+        """
+        Initialize maximal address length.
+        """
+        return REQUIRED_LITECOIN_ADDRESS_LENGTH
