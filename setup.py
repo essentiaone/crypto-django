1469b62575a8cebd290c3cbb25c7cff3a9f62cde
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-09-06 12:24:03 +0300

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='crypto-django',
-    version='0.0.1',
+    version='0.1.0',
     packages = find_packages(),
     include_package_data=True,
     license='MIT',
